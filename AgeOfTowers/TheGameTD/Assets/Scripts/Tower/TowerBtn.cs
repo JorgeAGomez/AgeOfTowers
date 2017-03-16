47bfb429c0573ef4d8ee76690a23ce9c6c71bedb
Jorge <jorge_gomez12@hotmail.com>
2017-03-16 16:23:31 -0600

    Game updated
    new UI design. new features added.

---
+++ AgeOfTowers/TheGameTD/Assets/Scripts/Tower/TowerBtn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBtn : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject towerObject;
+    [SerializeField]
+    private Sprite dragSprite;
+    [SerializeField]
+    private int towerPrice;
+
+    public GameObject TowerObject
+    {
+
+        get
+        {
+            return towerObject;
+        }
+    }
+
+    public Sprite DragSprite
+    {
+        get
+        {
+            return dragSprite;
+        }
+    }
+
+    public int TowerPrice
+    {
+        get
+        {
+            return towerPrice;
+        }
+    }
+}
